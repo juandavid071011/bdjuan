871b766eaa14939e2f8f3495e5f9c5ed7163928e
Juan David Alonso Cuellar <juan.david071011@gmail.com>
2026-09-23 16:51:25 -0500

    Configura conexion con backend

--- frontend_con_react/frontend/src/services/api.js
+++ frontend_con_react/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: 'https://bdjuan-production.up.railway.app',
     timeout: 5000
 });
 
